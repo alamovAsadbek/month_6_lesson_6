de3f8fb72186372ce92a18429d3d1fcf10b3566a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-02 16:17:39 +0500

    change

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from users import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
