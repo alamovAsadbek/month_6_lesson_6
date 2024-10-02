482efae99844a65ee4192e648a30625533a2a47c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-02 16:58:56 +0500

    change

--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from users.form import RegisterForm
+
 
 def register_view(request):
     if request.method == "POST":
-        pass
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user['is_active'] = False
+            user.save()
+            return render(request, "auth/login.html")
+
     return render(request, "auth/register.html")
 
 
