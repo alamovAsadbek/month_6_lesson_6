4f87e0a50606708ed905ebfac7e85be4e2ce570d
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-02 16:16:59 +0500

    change

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def register_view(request):
+    return HttpResponse("Register Page")
+
+
+def login_view(request):
+    return HttpResponse("Login Page")
+
+
+def logout_view(request):
+    return HttpResponse("Logout Page")
