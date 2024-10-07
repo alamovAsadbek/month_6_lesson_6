d9488383ad363c4ac9102952e7fb3c2d651077ed
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-07 16:50:19 +0500

    change

--- conf/urls.py
+++ conf/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -11,5 +12,10 @@ urlpatterns = [
     path('users/', include('users.urls', namespace="users")),
 ]
 
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('form/', include('forms.urls', namespace="form")),
+    path('', include('news.urls', namespace="news"))
+)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
